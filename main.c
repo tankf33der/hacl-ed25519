cb5d2503003ab6e3e56cf152d0366ab786a9c276
Mike <tankf33der@disroot.org>
2021-04-01 15:25:25 +0300

    .

--- main.c
+++ main.c
@@ -9,27 +9,15 @@
     uint8_t name[size]; \
     for(size_t i = 0; i < size; i++) name[i] = i;
 
-//@ ensures \result == 0;
-int sign_check_ed25519(void) {
-    ARRAY(hash1, 64);
-    ARRAY(hash2, 64);
-    ARRAY(key,   32);
-    ARRAY(pub1,  32);
-    ARRAY(pub2,  32);
-    ARRAY(in,    32);
-    int status = 0;
-    
-	Hacl_Ed25519_secret_to_public(pub2, key);
-    Hacl_Ed25519_sign(hash2, key, 32, in);
-    Hacl_Ed25519_verify(pub2, 32, in, hash2);	// as bool: 1 - ok, 0 - wrong
-    return status;
-}
-
 int main(void) {
-	int status = 0;
-	
-	sign_check_ed25519();
+    ARRAY(hash, 64);
+    ARRAY(key,  32);
+    ARRAY(pub,  32);
+    ARRAY(in,   32);
+    
+	Hacl_Ed25519_secret_to_public(pub, key);
+    Hacl_Ed25519_sign(hash, key, 32, in);
+    Hacl_Ed25519_verify(pub, 32, in, hash);
 
-	printf("%s\n", status != 0 ? "FAIL" : "OK");	
-	return status;
+	return 0;
 }
